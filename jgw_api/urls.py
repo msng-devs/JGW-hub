52990df896653043b1bd1d8ec19f7689f8a52172
K1A2 <jckim0307@gmail.com>
2022-08-06 15:28:45 +0900

    [FEAT] category get api 완료 #7

--- jgw_api/urls.py
+++ jgw_api/urls.py
@@ -1,15 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework import routers
 
 # app_name = 'jgw_api'
-#
-# router = routers.SimpleRouter()
-# router.register(r'testclass/', views.TestHeader, basename='TestClass')
 
-# urlpatterns = router.urls
+router = routers.DefaultRouter()
+router.register(r'category', views.CategoryViewSet, basename='categoty')
 
 urlpatterns = [
     path('test/', views.test_header),
-    path('testclass/', views.TestHeader.as_view())
+    path('testclass/', views.TestHeader.as_view()),
+    path('', include(router.urls))
 ]
--- jgw_api/views.py
+++ jgw_api/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework import viewsets, status, views
 from rest_framework.response import Response
-import json
 
-__user_tocken = 'user_uid'
+from .models import Category
+from .serializers import CategorySerializer
 
 @api_view(['GET'])
 def test_header(request):
     print(request.body)
-    # data = json.loads(request.body)
-    # print(data['member']['id'])
     return Response(data={'test': 'test'}, status=status.HTTP_200_OK)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        pass
 
 class TestHeader(views.APIView):
     def get(self, request):
