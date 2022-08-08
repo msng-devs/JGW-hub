892ac554aa06b5f4fcbfa07188a2bc691e53420d
K1A2 <jckim0307@gmail.com>
2022-08-08 21:11:16 +0900

    [FEAT] category delete unit test 구현 #7

--- jgw_api/tests.py
+++ jgw_api/tests.py
@@ -10,16 +10,42 @@ class CategoryApiTest(APITestCase):
     def setUp(self):
         self.url = '/hubapi/category/'
 
-    def test_category_get_api(self):
+    def category_get_api(self):
+        print("Category GET ALL")
         response: Response = self.client.get(self.url)
-        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print('GET data:', response.data)
+        return response.data
 
-    def test_category_post_api(self):
+    def category_post_api(self, content):
+        print("Category POST")
         data = {
-            "category_name": "Rust"
+            "category_name": content
         }
         response: Response = self.client.post(self.url, data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        response: Response = self.client.get(self.url)
-        print(response.data)
+
+    def category_get_by_id(self, key):
+        print("Category GET BY ID")
+        url = self.url + str(key) + '/'
+        response: Response = self.client.get(url)
+        print("GET Rust:", response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def category_delete_by_id(self, key):
+        print("Category DELETE BY ID")
+        url = self.url + str(key) + '/'
+        response: Response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_api_ok(self):
+        self.category_post_api('Rust')
+        self.category_post_api('Java')
+        self.category_post_api('Python')
+        data = self.category_get_api()
+        for i in data:
+            key = i['category_id_pk']
+            if i['category_name'] == 'Rust':
+                break
+        self.category_get_by_id(key)
+        self.category_delete_by_id(key)
