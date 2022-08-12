568f1399f28d20711306b5413f6cc753e5f3713e
K1A2 <jckim0307@gmail.com>
2022-08-12 15:56:43 +0900

    [FEAT] category get pagination 리턴 json 형식 변경 #6

--- jgw_api/custom_pagination.py
+++ jgw_api/custom_pagination.py
@@ -1,4 +1,19 @@
 from rest_framework.pagination import PageNumberPagination
+from collections import OrderedDict
+
+from rest_framework.response import Response
+
 
 class CategoryPageNumberPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 2
+
+    def get_paginated_response(self, data):
+        previous = self.get_previous_link()
+        if previous is not None and previous.endswith('category/'):
+            previous += '?page=1'
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', previous),
+            ('results', data)
+        ]))
