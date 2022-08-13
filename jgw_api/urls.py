6c544f006e74c818264b06353f1f2a94178e9cb9
K1A2 <jckim0307@gmail.com>
2022-08-13 18:27:04 +0900

    [DOCS] API 문서 추가 #11

--- jgw_api/urls.py
+++ jgw_api/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
 from . import views
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 # app_name = 'jgw_api'
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="자람 Hub ",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.IsAdminUser],
+)
 
 router = routers.DefaultRouter()
 router.register(r'category', views.CategoryViewSet, basename='categoty')
@@ -10,3 +26,10 @@ router.register(r'category', views.CategoryViewSet, basename='categoty')
 urlpatterns = [
     path('', include(router.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    ]
