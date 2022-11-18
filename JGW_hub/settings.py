a0d0343c55e9bed34ad8311883d22f5c4215c9fd
K1A2 <jckim0307@gmail.com>
2022-11-19 01:38:15 +0900

    [CHORE] add cors middleware

--- JGW_hub/settings.py
+++ JGW_hub/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
     'jgw_api',
 ]
 
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'JGW_hub.urls'
