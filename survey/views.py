1fbbf29f3476e7f1e463fbbc03c2d7cd54cababa
K1A2 <jckim0307@gmail.com>
2023-02-04 03:22:02 +0900

    [FEAT] 멀티 DB 설정 롤백 #79

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from secrets_content.files.secret_key import *
+import pymongo
+
+client = pymongo.MongoClient(**SURVEY_DATABASES)
+
+db = client.get_database('survey')
+post_collection = db.get_collection('survey_post')
 
 def test(request):
     return HttpResponse('test')
