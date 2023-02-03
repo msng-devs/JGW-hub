fd9f5c9f11c2dba89d046df23ecaf7bb67245ded
K1A2 <jckim0307@gmail.com>
2023-02-04 00:04:40 +0900

    [FEAT] 설문조사 models.py 클래스 생성 #79

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from djongo import models
+
+
+class MultiSelectSurvey(models.Model):
+    _id = models.ObjectIdField()
+
+    class Meta:
+        db_table = 'multi_select_survey'
+
+class SingleSelectSurvey(models.Model):
+    _id = models.ObjectIdField()
+
+    class Meta:
+        db_table = 'single_select_survey'
+
+class ShortAnswerSurvey(models.Model):
+    _id = models.ObjectIdField()
+
+    class Meta:
+        db_table = 'short_answer_survey'
+
