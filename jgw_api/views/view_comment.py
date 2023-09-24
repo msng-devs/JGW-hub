11db8ba56b3d9551bc3b69c8fd27c4c12f412de0
jeno0228 <113761913+jeno0228@users.noreply.github.com>
2023-09-24 15:29:09 +0900

    Update view_comment.py

--- jgw_api/views/view_comment.py
+++ jgw_api/views/view_comment.py
@@ -76,77 +76,67 @@ class CommentViewSet(viewsets.ModelViewSet):
     # post
     def create(self, request, *args, **kwargs):
         checked = request_check_admin_role(request)
-        try:
-            if isinstance(checked, Response):
+     
+        if isinstance(checked, Response):
                 # user role, 최소 admin role 중 하나라도 없으면 500 return
-                return checked
+            return checked
 
-            user_uid, user_role_id, admin_role_pk = checked
-            request_data = request.data
-            comment_serializer = CommentWriteSerializer(data=request_data)
+        user_uid, user_role_id, admin_role_pk = checked
+        request_data = request.data
+        comment_serializer = CommentWriteSerializer(data=request_data)
 
-            comment_serializer.is_valid(raise_exception=True)
-            logger.debug(f'{user_uid} Comment data verified')
-            member_member_pk = Member.objects.get(member_pk=user_uid)
-            comment = Comment(
-                comment_depth=comment_serializer.validated_data['comment_depth'],
-                comment_content=comment_serializer.validated_data['comment_content'],
-                comment_delete=comment_serializer.validated_data['comment_delete'],
-                post_post_id_pk=comment_serializer.validated_data['post_post_id_pk'],
-                member_member_pk=str(member_member_pk),
-                comment_comment_id_ref=comment_serializer.validated_data['comment_comment_id_ref']
+        comment_serializer.is_valid(raise_exception=True)
+        logger.debug(f'{user_uid} Comment data verified')
+        member_member_pk = Member.objects.get(member_pk=user_uid)
+        logger.debug(f'{member_member_pk} found')
+        comment = Comment(
+            comment_id=comment_serializer.validated_data['comment_id'],
+            comment_depth=comment_serializer.validated_data['comment_depth'],
+            comment_content=comment_serializer.validated_data['comment_content'],
+            comment_write_time=comment_serializer.validated_data['comment_write_time'],
+            comment_update_time=comment_serializer.validated_data['comment_updated_time'],
+            comment_delete=comment_serializer.validated_data['comment_delete'],
+            post_post_id_pk=comment_serializer.validated_data['post_post_id_pk'],
+            member_member_pk=str(member_member_pk),
+            comment_comment_id_ref=comment_serializer.validated_data['comment_comment_id_ref']
 
-            )
-            logger.debug(f'{comment}')
-            post_instance = comment_serializer.validated_data['post_post_id_pk']
-            board_instance = post_instance.board_boadr_id_pk
-            if user_role_id >= admin_role_pk or user_role_id >= board_instance.role_role_pk_comment_write_level.role_pk:
-                logger.debug(f'{user_uid} Comment post approved\nrequest_body= {comment}')
-                self.perform_create(comment)
-                comment_pk = comment_serializer.data['comment_id']
-                responses_instance = Comment.objects.get(comment_id=comment_pk)
-                serializer = CommentWriteResultSerializer(responses_instance)
-                responses_data = serializer.data
-                update_log = f'{user_uid} Comment data created' \
-                             f'\tkey: {responses_data["comment_id"]} created log'
-                for k in responses_data.keys():
-                    instance_data = getattr(responses_instance, k)
-                    if k in ('comment_content',):
-                        instance_data = instance_data[:50]
-                    update_log += f'\n\t{k}: {instance_data}'
-                logger.info(update_log)
+        )
+        logger.debug(f'{comment}')
+        post_instance = comment_serializer.validated_data['post_post_id_pk']
+        board_instance = post_instance.board_boadr_id_pk
+        if user_role_id >= admin_role_pk or user_role_id >= board_instance.role_role_pk_comment_write_level.role_pk:
+            logger.debug(f'{user_uid} Comment post approved\nrequest_body= {comment}')
+            self.perform_create(comment)
+            comment_pk = comment_serializer.data['comment_id']
+            responses_instance = Comment.objects.get(comment_id=comment_pk)
+            serializer = CommentWriteResultSerializer(responses_instance)
+            responses_data = serializer.data
+            update_log = f'{user_uid} Comment data created' \
+                         f'\tkey: {responses_data["comment_id"]} created log'
+            for k in responses_data.keys():
+                instance_data = getattr(responses_instance, k)
+                if k in ('comment_content',):
+                    instance_data = instance_data[:50]
+                update_log += f'\n\t{k}: {instance_data}'
+            logger.info(update_log)
 
-                return Response(responses_data, status=status.HTTP_201_CREATED)
-            else:
-                logger.info(f"{user_uid} Comment create denied")
-                responses_data = {
-                    "timestamp": datetime.datetime.now().isoformat(),
-
-                    "status": 403,
-
-                    "error": "Forbidden",
-
-                    "code": "JGW_hub-comment-002",
-
-                    "message": "Comment create denied",
-
-                    "path": "/hub/api/v1/comment/"
-                }
-                return Response(responses_data, status=status.HTTP_403_FORBIDDEN)
-        except Exception as e:
+            return Response(responses_data, status=status.HTTP_201_CREATED)
+        else:
+            logger.info(f"{user_uid} Comment create denied")
             responses_data = {
                 "timestamp": datetime.datetime.now().isoformat(),
 
-                "status": 500,
+                "status": 403,
 
-                "error": "Internal",
+                "error": "Forbidden",
 
-                "code": "temp",
+                "code": "JGW_hub-comment-002",
 
-                "message": str(e),
+                "message": "Comment create denied",
 
                 "path": "/hub/api/v1/comment/"
             }
+            return Response(responses_data, status=status.HTTP_403_FORBIDDEN)
 
     # patch
     def partial_update(self, request, *args, **kwargs):
