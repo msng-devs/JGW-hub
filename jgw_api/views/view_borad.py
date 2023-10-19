95b9a0b306ca9cc2b01289a4387cee14d873f0d8
jeno0228 <113761913+jeno0228@users.noreply.github.com>
2023-10-19 21:46:31 +0900

    Update view_borad.py

--- jgw_api/views/view_borad.py
+++ jgw_api/views/view_borad.py
@@ -113,7 +113,7 @@ class BoardViewSet(viewsets.ModelViewSet):
             instance = self.get_object()
             logger.debug('1')
             request_data = request.data
-            logger.debug('2')
+            logger.debug(f'[2] request_data.dict : {request_data.dict()}\nrequest_data.dict().keys() : {request_data.dict().keys()}')
             target_keys = list(request_data.dict().keys())
             logger.debug('3')
             before_change = dict()
